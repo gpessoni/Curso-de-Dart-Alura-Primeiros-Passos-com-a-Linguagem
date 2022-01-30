8636da9614bc2407237ad8d3e7b2627ae9e3aa79
gpessoni <pessonigs@gmail.com>
2022-01-30 11:45:33 -0300

    Create the folder from the second module of the course

---
+++ Módulo 02 - Instalação e o Primeiro Programa/Aula 01 - Instalando o Dart/Anotações.dart
@@ -0,0 +1,13 @@
+// Para instalar o Dart no Windows, precisamos instalar o Chocolatey que, basicamente, é um gerenciador de pacotes para o Windows, semelhante ao Homebrew do macOS e o apt do Linux.
+// Depois da instalação do Chocolatey, basta abrir o CMD do Windows em modo administrativo e executar o comando abaixo:
+//  choco install dart-sdk
+
+//Para sabermos se temos o dart instalado corretamente, abrimos nosso CMD e executamos o seguinte comando: dart --version 
+//Caso ele mostre a versão atual do Dart instalada na máquina, podemos saber que o mesmo foi instalado corretamente
+
+//Após a instalação do Dart em sua máquina, é importante verificar se o programa foi configurado em suas variáveis de ambiente, para que possamos ter acesso a ele a partir do nosso terminal.
+// As variáveis de ambiente permitem com que programas instalados em seu computador tenham um ponto de partida universal, ou seja, se tivermos uma variável de ambiente para o programa do Dart instalado em nossos computadores, teremos acesso facilitado a ele em nosso terminal, dando flexibilidade na hora de construir nossos programas.
+
+//Com isso, já teremos todo o pacote de desenvolvimento do Dart instalado em nosso computador  e as váriveis de ambiente confuiguradas
+//Após toda a instalação, já podemos criar arquivos .dart em nossos editores de código
+
---
+++ Módulo 02 - Instalação e o Primeiro Programa/Aula 03 - Nosso primeiro Programa/primeiro_programa.dart
@@ -0,0 +1,10 @@
+void main() {
+  print("Olá mundo");
+}
+
+
+
+//Main : significa que esse será o ponto de partida do nosso programa, e é uma função. Uma função não é exclusivamente um ponto de partida, mas nesse caso ela será o ponto de partida. Nenhum programa do Dart funcionará sem a main().
+//Print: Também é uma função e ficará responsável por imprimir na tela tudo que estiver entre os parênteses e entre as aspas, no nosso caso irá imprimir: Olá mundo
+
+//O que você escreve é chamado de código-fonte, e possui esse nome porque contém os códigos (ou comandos) que seu computador utiliza como fonte para atingir um objetivo, seja imprimir uma linha de texto na tela, seja realizar cálculos mirabolantes. Um programa pode conter dezenas de milhares de linhas de código, e você provavelmente nunca vai atingir o limite. Portanto, saiba que sempre terá o espaço necessário para instruir seu computador a realizar qualquer tarefa que ele possa realizar.
