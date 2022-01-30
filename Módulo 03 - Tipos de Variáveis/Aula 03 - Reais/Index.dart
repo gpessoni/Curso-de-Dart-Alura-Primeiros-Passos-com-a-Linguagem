0837ff7b08f12bde77d083a168106663cf35e277
gpessoni <pessonigs@gmail.com>
2022-01-30 20:34:51 -0300

    Update the folder form the Third module from project

---
+++ Módulo 03 - Tipos de Variáveis/Aula 03 - Reais/Index.dart
@@ -0,0 +1,17 @@
+void main() {
+  //Abaixo, estou declarando uma variável do tipo real
+  double salario = 1250.50;
+
+  //Aqui, utilizo o valor da primeira variável e multiplico por 2
+  double salarioProximoAno = salario * 2;
+
+  print("Meu salário é de R\$ ${salario}");
+  print("Meu salário ano que vêm será de R\$ ${salarioProximoAno}");
+
+//O tipo real, permite realizarmos operações no qual o resultado é um número quebrado
+  double divisao = 5 / 2;
+  print(divisao);
+
+  double multiplicacao = 4.55 * 4;
+  print(multiplicacao);
+}
