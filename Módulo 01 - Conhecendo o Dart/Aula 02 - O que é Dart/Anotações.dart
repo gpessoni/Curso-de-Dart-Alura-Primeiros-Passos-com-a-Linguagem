81f792594f050024e105260354aac36e6428444c
gpessoni <pessonigs@gmail.com>
2022-01-30 11:09:15 -0300

    Create the folders from the first module of the course

---
+++ Módulo 01 - Conhecendo o Dart/Aula 02 - O que é Dart/Anotações.dart
@@ -0,0 +1,13 @@
+// Sobre o Dart
+//  O Dart é uma linguagem de programação que nasceu em 2011 e foi desenvolvida pela famosa gigante da tecnologia Google. Ele foi construído desde o início pensando em desenvolvimento rápido e multiplataformas.
+// Geralmente, conhecemos o Dart por causa do Flutter, uma framework construída também pelo Google com o objetivo de dar suporte a desenvolvedores para desenvolverem aplicações nativas, tanto para Android quanto IOS. Com o Flutter também é possível desenvolver aplicações web e Desktop.
+// Para evitar confusões, vamos definir dessa forma: O Flutter é a ferramenta que utiliza essa linguagem, tornando possível o desenvolvimento de aplicações móveis. Logo, o Dart funciona sem o Flutter, mas o Flutter não funciona sem o Dart.
+
+// O Dart possui um compilador interno, que converte o código Dart em um código JavaScript, capaz de ser executado na web. Esse compliador recebe o nome de dart2js (Dart to JavaScript)
+// A principio, parece uma má ideia, pois poderíamos escrever o código em Javascript diretamente ao invés de usar o Dart e depois convertê-lo. Mas a tradução indica que o Google investiu em otimização interna e segundo dados da própria empresa o Dart compilado para Javascrip pode ter uma performance 50% melhor do que o V8, o interpretador de Javascript mais potente atualmente e que curiosamente também foi gerado pelo Google.]
+
+
+// Mas afinal, por que precisamos de uma nova linguagem de programação se podemos usar o Javascript programando para a web e o Java para dispositivos? A resposta é que já estava na hora de haver uma nova linguagem, pois o Dart não coleta funcionalidades redudantes através dos anos.
+// Tanto o Javascript quanto o Java, linguagem com as quais estamos comparando o Dart, foram lançadas em 1995. As duas possuem vestígios de desenvolvimento de mais de 20 anos. Isso quer dizer que a versão que podemos baixar e executar nesse momento possui códigos de mais de 20 anos, já que todas as novidades foram acopladas ao mesmo código ao longo do tempo.
+// O Dart foi criado em 2011 e desde sua fundação foi pensado para ter a melhor performance e abranger todos os dispositivos possíveis da atualidade, Esta é uma nova linguagem com um novo objetivo para alcançar um novo campo de desenvolvimento em um futuro próximo, tanto na web quanto em dispositivos móveis e Desktop.
+
