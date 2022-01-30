0a3c7e9df05aae11739d21c8a6ce1f6633139de1
gpessoni <pessonigs@gmail.com>
2022-01-30 20:50:01 -0300

    Update the folder form the Third module from project

--- Módulo 03 - Tipos de Variáveis/Aula 06- Strings/Index.dart
+++ Módulo 03 - Tipos de Variáveis/Aula 06- Strings/Index.dart
@@ -1,1 +1,14 @@
-void main() {}
+// void main() {
+//   int idade = 18;
+//   //Criando uma variável do tipo String
+//   String nome = "Gabriel";
+//   //Imprimindo o valor da varíavel nome
+// }
+
+void main() {
+  int ano = 2013;
+  String texto = "Alura - Cursos Online de Tecnología desde";
+  ano = ano + 6;
+
+  print("${texto} ${ano}");
+}
