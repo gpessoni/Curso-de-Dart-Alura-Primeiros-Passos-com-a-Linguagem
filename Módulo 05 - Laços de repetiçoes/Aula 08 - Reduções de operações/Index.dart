d3a67c53c4b09256866610cf44f386ce1b284d94
gpessoni <pessonigs@gmail.com>
2022-02-03 22:33:21 -0300

    Update the modules from the Fifth module of the course

---
+++ Módulo 05 - Laços de repetiçoes/Aula 08 - Reduções de operações/Index.dart
@@ -0,0 +1,10 @@
+// Você sabia que existem algumas operações no Dart que podem ser reduzidas?
+
+// Assim como variavel++ é uma redução de variavel = variavel + 1, o Dart nos permite fazer algumas reduções com outras operações:
+
+// variavel++ substitui variavel = variavel + 1
+// variavel-- substitui variavel = variavel - 1
+// variavel += outraVariavel substitui variavel = variavel + outraVariavel
+// variavel -= outraVariavel substitui variavel = variavel - outraVariavel
+// variavel *= outraVariavel substitui variavel = variavel * outraVariavel
+// variavel /= outraVariavel substitui variavel = variavel / outraVariavel
