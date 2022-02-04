2d474c8377b6a65fd7b731b8adcab74e13fc99ce
gpessoni <pessonigs@gmail.com>
2022-02-03 22:33:16 -0300

    Update the modules from the Fifth module of the course

---
+++ Módulo 05 - Laços de repetiçoes/Aula 05 - Repetições com o for/Index.dart
@@ -0,0 +1,28 @@
+//O loop for é uma implementação de um loop definido. O loop for executa o bloco de código por um número especificado de vezes. Ele pode ser usado para iterar sobre um conjunto fixo de valores, como uma matriz.
+
+// void main() {
+// //for (variável ; condição ; o que fazer com essa variável)
+//   int contador;
+
+//   for (contador = 10; contador <= 0; contador--) {
+//     print("O valor do contador é $contador");
+//   }
+//   print("Finalizando o programa com o valor do contador:  $contador");
+// }
+
+// void main() {
+// //for (variável ; condição ; o que fazer com essa variável)
+//   int contador = 0;
+
+//   for (contador; contador <= 10; contador++) {
+//     print("O valor do contador é $contador");
+//   }
+//   print("Finalizando o programa com o valor do contador:  $contador");
+// }
+
+void main() {
+//for (variável ; condição ; o que fazer com essa variável)
+  for (int contador = 0; contador <= 10; contador++) {
+    print("O valor do contador é $contador");
+  }
+}
