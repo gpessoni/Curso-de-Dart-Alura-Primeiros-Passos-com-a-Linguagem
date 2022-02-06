0f5456463f3b2975c3e92423ea9fab84e3b5de6b
gpessoni <pessonigs@gmail.com>
2022-02-05 21:48:41 -0300

    Update the modules from the Fifth module of the course

--- Módulo 04 - Condicionais/Aula 01 - Condicional If/Index.dart
+++ Módulo 04 - Condicionais/Aula 01 - Condicional If/Index.dart
@@ -1,15 +1,15 @@
-void main() {
-  print("Testando condicionais");
-  int idade = 17;
+// void main() {
+//   print("Testando condicionais");
+//   int idade = 17;
 
-//Condicional if e else:O if e o else são comandos que verificam determinada condição na programação. O uso do if em um programa em Dart visa verificar se determinada ação é verdadeira e executar o bloco de código contido em seu escopo.
-//No trecho de código abaixo, estamos impondo uma condição de entradas, no caso ter 18 anos. Caso o usuário tenha 18 anos ou mais, pode entraer. Caso o mesmo seja menor de idade, não poderá entrar.
-  if (idade > 18) {
-    print("Você é maior de idade, pode entrar!");
-  } else {
-    print("Você  é menor de idade, não pode entrar!");
-  }
-}
+// //Condicional if e else:O if e o else são comandos que verificam determinada condição na programação. O uso do if em um programa em Dart visa verificar se determinada ação é verdadeira e executar o bloco de código contido em seu escopo.
+// //No trecho de código abaixo, estamos impondo uma condição de entradas, no caso ter 18 anos. Caso o usuário tenha 18 anos ou mais, pode entraer. Caso o mesmo seja menor de idade, não poderá entrar.
+//   if (idade > 18) {
+//     print("Você é maior de idade, pode entrar!");
+//   } else {
+//     print("Você  é menor de idade, não pode entrar!");
+//   }
+// }
 
 //Comparativos
 // >  :Maior que
@@ -17,3 +17,14 @@ void main() {
 // == :Igual a 
 // <= :Menor igual
 // >= Maior igual
+
+import 'dart:io';
+void main()
+{
+  print("Digite o valor de A");
+    int a = int.parse(stdin.readLineSync()); 
+print("Digite o valor de B");
+      int b = int.parse(stdin.readLineSync()); 
+int soma = a + b;
+print(soma);
+}
