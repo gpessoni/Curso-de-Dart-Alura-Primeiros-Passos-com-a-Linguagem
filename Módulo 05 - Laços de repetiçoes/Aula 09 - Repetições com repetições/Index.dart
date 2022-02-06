271c5ffa4ca3d71585eef38a163f22ce785e021c
gpessoni <pessonigs@gmail.com>
2022-02-05 21:48:43 -0300

    Update the modules from the Fifth module of the course

---
+++ Módulo 05 - Laços de repetiçoes/Aula 09 - Repetições com repetições/Index.dart
@@ -0,0 +1,36 @@
+// // void main() {
+// //     for(int contador = 1;contador<=10;contador++){
+// //     int resultado = 2 * contador;
+// //     print("2 * $contador =  $resultado");
+// //     }
+// // }
+
+// void main() {
+//     for(int multiplicando = 1;multiplicando<=10;multiplicandor++){
+//         print("Tabuada de multiplicando");
+
+//         for(int contador = 1;multiplicando<=10;multiplicandor++)
+//             print("$multiplicando * ${multiplicando * contador}");
+//         }
+//     }
+// }
+
+// void main() {
+//     for(int multiplicando = 1;multiplicando<=10;multiplicandor++){
+//         for(int contador = 1;multiplicando<=10;multiplicandor++)
+//             int resultado = multiplicando * contador;
+//             print("$multiplicando * $contador =  $resultado");
+//         }
+//     }
+// }
+
+void main() {
+    for(int multiplicando = 1;multiplicando<=10;multiplicandor++){
+        print("Tabuada de multiplicando")`.
+
+        for(int contador = 1;multiplicando<=10;multiplicandor++)
+            // int resultado = multiplicando * contador;
+            print("$multiplicando * ${multiplicando * contador}");
+            }
+        }
+}
